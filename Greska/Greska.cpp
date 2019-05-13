2d6c7dd2bb1d1d5c1c2b1dfe6728e7434216a47d
unknown <vedran.vinkovic2@gmail.com>
2019-05-13 16:12:33 +0100

    Error reporting
    -> dodane klase za opću, leksičku i sintaksnu grešku
    -> promijenjen kod koji ispisuje greške da koristi navedene klase

---
+++ Greska/Greska.cpp
@@ -0,0 +1,39 @@
+﻿#include "Greska.hpp"
+
+namespace C0Compiler
+{
+	Greska::Greska(std::string const& vrsta, int redak, int stupac)
+		: poruka(vrsta + "! Redak: " + std::to_string(redak) +
+			", stupac: " + std::to_string(stupac) + ". Opis: ") {}
+
+	Greska::Greska(std::string const& vrsta, int redak, int stupac, std::string const& opis) 
+		: poruka(vrsta + "! Redak: " + std::to_string(redak) +
+		", stupac: " + std::to_string(stupac) + ". Opis: " + opis + ".") {}
+
+	LeksickaGreska::LeksickaGreska(int redak, int stupac, char dobio, char ocekujem) : Greska("Leksička greška", redak, stupac)
+	{
+		poruka << "neočekivan znak '"<< dobio <<"', očekujem '" << ocekujem << "'.";
+	}
+
+	LeksickaGreska::LeksickaGreska(int redak, int stupac, char znak) : Greska("Leksička greška", redak, stupac)
+	{
+		poruka << "nepoznat znak: '" << znak << "'";
+	}
+
+	SintaksnaGreska::SintaksnaGreska(int redak, int stupac, Token const& dobio, enum TokenTip ocekujem) :Greska("Sintaksna greška", redak, stupac)
+	{
+		poruka << "neočekivan token '" << dobio << "', očekujem '" << tokenString[ocekujem] << "'.";
+	}
+
+	SintaksnaGreska::SintaksnaGreska(int redak, int stupac, Token const& dobio, std::list<TokenTip>&& ocekujem) :Greska("Sintaksna greška", redak, stupac)
+	{
+		poruka << "neočekivan token '" << dobio << "', očekujem jedan od: ";
+		for (std::list<TokenTip>::iterator it = ocekujem.begin(); it != ocekujem.end();)
+		{
+			poruka << "'" << tokenString[*it] << "'";
+			if (++it != ocekujem.end())
+				poruka << ", ";
+		}
+	}
+
+}
---
+++ Greska/Greska.hpp
@@ -0,0 +1,49 @@
+﻿#ifndef GRESKA_HPP
+#define GRESKA_HPP
+
+#include <string>
+#include <iostream>
+#include <stdexcept>
+#include <sstream>
+#include <list>
+
+#include "../Token/Token.hpp"
+
+namespace C0Compiler
+{
+	class Greska : std::exception
+	{
+		protected:
+			std::stringstream poruka;
+
+		public:
+			Greska() = delete;
+			Greska(std::string const& vrsta, int redak, int stupac);
+			Greska(std::string const& vrsta, int redak, int stupac, std::string const& opis);
+			virtual const char* what() const override { return poruka.str().c_str(); };
+			virtual ~Greska() {}
+	};
+
+	class LeksickaGreska : public Greska
+	{
+		public:
+			LeksickaGreska() = delete;
+			LeksickaGreska(int redak, int stupac, char dobio, char ocekujem);
+			LeksickaGreska(int redak, int stupac, std::string const& opis) : Greska("Leksička greška", redak, stupac, opis) {}
+			LeksickaGreska(int redak, int stupac, char znak);
+			virtual ~LeksickaGreska(){}
+	};
+
+	class SintaksnaGreska : public Greska
+	{
+		public:
+			SintaksnaGreska() = delete;
+			SintaksnaGreska(int redak, int stupac, std::string const& opis) : Greska("Sintaksna greška", redak, stupac, opis) {}
+			SintaksnaGreska(int redak, int stupac, Token const& dobio, enum TokenTip ocekujem);
+			SintaksnaGreska(int redak, int stupac, Token const& dobio, std::list<TokenTip>&& ocekujem);
+
+			virtual ~SintaksnaGreska(){}
+	};
+}
+
+#endif
